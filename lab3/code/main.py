34c4696a4e6f23183fdffda7df0a38809d795521
Oscar Hsu <vm3y3rmp40719@gmail.com>
2023-04-10 22:27:53 +0800

    Add testing for Lab 3

--- lab3/code/main.py
+++ lab3/code/main.py
@@ -13,7 +13,12 @@ def main(config: dict, is_logging=True) -> None:
     assert dir_dataset.exists(), \
         f"Dataset {dir_dataset} doesn't exist (option: {dir_dataset})."
     
-    Solver(config)
+    
+    solver = Solver(config)
+    if config.mode == 'train':
+        solver.train_all()
+    else: # 'test'.
+        solver.eval_all()
     
 if __name__ == '__main__':
     # For CUDA debugging.
@@ -21,15 +26,17 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     
+    parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'], 
+        help='Mode. Options: "train" (default) or "test".')
     parser.add_argument('--model', type=str, default='eegnet', choices=['eegnet', 'deepconvnet'], 
         help='Model. Options: "eegnet" (EEGNet, default), or "deepconvnet" (DeepConvNet).')
-    parser.add_argument('--gpu', type=lambda s: [int(item) for item in s.split(',')], default=[2],  
+    parser.add_argument('--gpu', type=lambda s: [int(item) for item in s.split(',')], default=[4],  
         help='GPUs.')
 
     """
     Data configs.
     """
-    parser.add_argument('--batch_size', type=int, default=64, 
+    parser.add_argument('--batch_size', type=int, default=128, 
         help='Mini-batch size.')
     parser.add_argument('--num_workers', type=int, default=4, 
         help='Number of subprocesses to use for data loading.')
@@ -41,6 +48,8 @@ if __name__ == '__main__':
         help='Directory of dataset.')
     parser.add_argument('--dir_output', type=str, default='./output/', 
         help='Directory of outputs.')
+    parser.add_argument('--dir_model', type=str, default='./models/', 
+        help='Directory of model weights.')
     parser.add_argument('--path_acc', type=str, default='./acc.txt', 
         help='Path of accuracy output file.')
 
@@ -57,15 +66,17 @@ if __name__ == '__main__':
     """
     Training configs.
     """
-    parser.add_argument('--num_epochs', type=int, default=300,  
+    parser.add_argument('--num_epochs', type=int, default=500,  
         help='Number of epochs for training.')
-    parser.add_argument('--lr', type=float, default=1e-2, 
+    parser.add_argument('--lr', type=float, default=5e-3, 
         help='Learning rate.')
     parser.add_argument('--seed', type=int, default=42, 
         help='Random seed.')
     parser.add_argument('--optim', type=str, default='adam', choices=['adam', 'adamw', 'sgd'], 
         help='Optimizer. Options: "adam" (default), "adamw", or "sgd" (learning rate is auto 100x).')
-    parser.add_argument('--dropout', type=float, default=0.25, 
+    parser.add_argument('--dropout', type=float, default=0.5, 
+        help='Dropout.')
+    parser.add_argument('--resume_ckpt', type=str, default='', 
         help='Dropout.')
 
     main(parser.parse_args(), is_logging=True)
--- lab3/code/src/solver.py
+++ lab3/code/src/solver.py
@@ -29,18 +29,21 @@ class Solver(object):
         self.device = torch.device(f'cuda:{config.gpu[0]}' if torch.cuda.is_available() else 'cpu')
         os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(list(map(str, config.gpu)))
         
-        print(f'\n***\nCurrently using {self.device}...\n***\n')
+        print(f'***\nCurrently using {self.device}...\n***\n')
         
         # Training configs.
+        self.model = config.model
         self.num_epochs = config.num_epochs
         self.lr = config.lr
         self.optim = config.optim
         self.dropout = config.dropout
+        self.batch_size = config.batch_size
         
         # Misc.
         self.timestamp = utils.get_timestamp(datetime.now())
         self.epoch_log = config.epoch_log
         self.is_plot_saving = config.is_plot_saving
+        self.path_acc = config.path_acc
         
         if self.is_plot_saving: 
             self.mkdirs(config)
@@ -48,18 +51,23 @@ class Solver(object):
         # Build model.
         self.build_model(config)
         
-        # Train.
+        # Load model.
+        if config.resume_ckpt.strip() != '':
+            self.load_model(config.resume_ckpt)
+        
+    def train_all(self) -> None: 
         for setup in self.models:
             model = setup['model']
             optim = setup['optim']
             scheduler = setup['scheduler']
             criterion = setup['criterion']
+            act = setup['act']
             
-            self.train(model, optim, scheduler, criterion)
+            self.train(model, optim, scheduler, criterion, act)
             
         # Plot.
-        path = (f'{config.model}_{self.optim}'
-                f'_{config.batch_size}_{self.lr}_{self.dropout}')
+        path = (f'{self.model}_{self.optim}'
+                f'_{self.batch_size}_{self.lr}_{self.dropout}')
         model_loss = []
         model_loss += self.model_train_loss
         model_loss += self.model_test_loss
@@ -74,17 +82,44 @@ class Solver(object):
             model_acc, self.num_epochs, Path(self.dir_output, f'{path}_acc.png'), 
             'Accuracy', self.labels, 'lower right')
         
-        id = (f'{config.model}_{self.optim}'
-            f'_{config.batch_size}_{self.lr}_{self.dropout}')
-        with open(config.path_acc, 'a') as file:
+        id = (f'{self.model}_{self.optim}'
+            f'_{self.batch_size}_{self.lr}_{self.dropout}')
+        with open(self.path_acc, 'a') as file:
             for act, acc in zip(self.acts, self.model_train_acc):
                 file.write(f'({id}_{act}) training accuracy: {max(acc) * 100:.2f}%.\n')
             for act, acc in zip(self.acts, self.model_test_acc):
                 file.write(f'({id}_{act}) testing accuracy: {max(acc) * 100:.2f}%.\n')
                 
             file.write(f'{"-" * 67}\n')
+    
+    def eval_all(self) -> None:
+        num_test_data = len(self.test_loader.dataset)
+        
+        for setup in self.models:
+            model = setup['model']
+            criterion = setup['criterion']
+            act = setup['act']
             
-    def train(self, model, optim, scheduler, criterion) -> None:
+            model.eval()
+            with torch.no_grad():
+                test_total_loss = 0
+                test_total_acc = 0
+                for data, label in self.test_loader:
+                    data = data.to(self.device)
+                    label = label.to(self.device)
+                    
+                    pred = model(data)
+                    
+                    loss = criterion(pred, label)
+                    test_total_loss += loss
+                    test_total_acc += self.cal_acc(label, pred)
+                
+                test_mean_loss = test_total_loss / num_test_data
+                test_mean_acc = test_total_acc / num_test_data
+                
+                print(f'{act:>9}: test_loss: {test_mean_loss:>7.5f}, test_acc: {test_mean_acc:>4.2f}.')
+        
+    def train(self, model, optim, scheduler, criterion, act: str) -> None:
         num_train_data = len(self.train_loader.dataset)
         num_test_data = len(self.test_loader.dataset)
         
@@ -160,13 +195,16 @@ class Solver(object):
 
         print(f'\n***\nElapsed time (hh:mm:ss.ms): {timedelta(seconds=time_end - time_start)}.\n***')
         
+        self.save_model(model, act, self.num_epochs)
+        
     def build_model(self, config: dict) -> None: 
         self.acts = ['relu', 'leakyrelu', 'elu']
         
         # Model.
         model = Eegnet if config.model == 'eegnet' else DeepConvNet
         self.models = [
-            {'model': model(act_name=act, dropout=self.dropout).to(self.device)}
+            {'model': model(act_name=act, dropout=self.dropout).to(self.device), 
+             'act': act}
             for act in self.acts
         ]
         
@@ -207,6 +245,21 @@ class Solver(object):
             for act in self.acts
         ]
         
+    def load_model(self, dir_ckpt) -> None:
+        try: 
+            for act, setup in zip(self.acts, self.models): 
+                setup['model'].load_state_dict(
+                    torch.load(Path(dir_ckpt, f'{act}_{self.num_epochs}.pt'), map_location='cpu'))
+                setup['model'] = setup['model'].to(self.device)
+            
+            print(f'Finished loading model from {dir_ckpt}...\n')
+        except Exception as msg:
+            print(f'Failed to load checkpoint from {dir_ckpt}. Message: \n{msg}.\n')
+        
+    def save_model(self, model, act: str, epoch: int) -> None:
+        torch.save(model.state_dict(), 
+            Path(self.dir_model, f'{act}_{epoch}.pt'))
+        
     def cal_acc(self, real: torch.tensor, pred: torch.tensor) -> float: 
         return (torch.max(pred, 1)[1] == real).sum().item()
             
@@ -235,7 +288,12 @@ class Solver(object):
         path = (f'{self.timestamp}_{config.model}_{self.optim}'
                 f'_{config.batch_size}_{self.lr}_{self.dropout}')
         
-        self.dir_output = Path(config.dir_output, path)
-        if not self.dir_output.is_dir(): 
-            self.dir_output.mkdir(parents=True, exist_ok=True)
+        if config.mode == 'train':
+            self.dir_output = Path(config.dir_output, path)
+            if not self.dir_output.is_dir(): 
+                self.dir_output.mkdir(parents=True, exist_ok=True)
+            
+            self.dir_model = Path(config.dir_model, path)
+            if not self.dir_model.is_dir(): 
+                self.dir_model.mkdir(parents=True, exist_ok=True)
             
